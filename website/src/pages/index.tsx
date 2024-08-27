dccbdbcaad1665ff5f143cbbca61b01928c7385e
Karthikeyan Ranasthala <karthikeyan.ranasthala@gmail.com>
2024-08-27 19:11:46 +0530

    chore: switch theme to system

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
   return (
     <div className="wrapper light">
       <Head />
-      <Toaster theme="light" richColors={richColors} closeButton={closeButton} expand={expand} position={position} />
+      <Toaster theme="system" richColors={richColors} closeButton={closeButton} expand={expand} position={position} />
       <main className="container">
         <Hero />
         <div className="content">
